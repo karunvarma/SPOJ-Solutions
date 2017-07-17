bacf01fd97dcb56477be41fb620ad125c1a959c1
Praval Singhal <praval.singhal18@gmail.com>
2017-07-17 13:32:12 +0530

    Create HUBULLU.cpp

---
+++ SPOJ/HUBULLU.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	long long t;
+	cin>>t;
+	while(t--)
+	{
+		long long n,s;
+		cin>>n>>s;
+		if(s==0)
+		cout<<"Airborne wins.\n";
+		else
+		cout<<"Pagfloyd wins.\n";
+	}
+}
